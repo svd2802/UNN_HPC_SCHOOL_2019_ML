ed649af01593b59e0d446896e32ad43b094dc195
svd2802 <narutoche@bk.ru>
2019-11-20 05:11:07 +0600

    Pracctice 1. 20.11 3:10 (NOT READY)

--- samples/practice1_sumin_viktor.py
+++ samples/practice1_sumin_viktor.py
@@ -9,9 +9,9 @@ from imagefilter import ImageFilter
 def build_argparse():
     parser=argparse.ArgumentParser()
     
-    parser.add_argument('-in', '--input', help = 'your cmd \ input ', type = str)
-    #parser.add_argument('-w', '--width', help = 'your cmd \ width ', type = str, required = False)
-    #parser.add_argument('-h', '--high', help = 'your cmd \high ', type = str, required = False)
+    parser.add_argument('-i', '--input', help = 'your cmd \ input ', type = str)
+    parser.add_argument('-w', '--width', help = 'your cmd \ width ', type = int, default = 600, required = False)
+    parser.add_argument('-l', '--high', help = 'your cmd \high ', type = int, default = 600, required = False)
     #
     # Add your code here
     #
@@ -25,10 +25,23 @@ def main():
     
     image_path = args.input
     log.info(image_path)
-    print(str(image_path))
     
-    image = cv2.imread("image_path")
-    cv2.imshow("Image", image)
+    image = cv2.imread(image_path)
+    log.info(image.shape)
+    
+    filter_gray = ImageFilter(gray = True, shape = (image.shape[0], image.shape[1]), crop = False)
+    image_gray = filter_gray.process_image(image)
+    cv2.imshow("Gray", image_gray)
+    
+    filter_shape = ImageFilter(gray = False, shape = (args.width, args.high), crop = False)
+    image_shape = filter_shape.process_image(image)   
+    cv2.imshow("Shape", image_shape)
+    
+    filter_crop = ImageFilter(gray = False, shape = (image.shape[0], image.shape[1]), crop = True)
+    image_crop = filter_crop.process_image(image)
+    cv2.imshow("Image", image_crop)
+    
+    log.info(image_crop.shape)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     #
--- src/imagefilter.py
+++ src/imagefilter.py
@@ -2,17 +2,26 @@ import cv2
 
 class ImageFilter():
     def __init__(self, gray = False, shape = None, crop = None):
+        
         self.gray = gray
         if shape:
             self.shape = shape
-        if crop:
-            self.crop = crop
+        self.crop = crop
     
     def process_image(self, image):
-        #
-        # Put your code here, like example
-        #
-        #if self.shape:
-        #    do something
-        # 
+        if self.shape:
+            image = cv2.resize(image, dsize = self.shape)
+            
+        if self.gray:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            
+        if self.crop:
+            center = (self.shape[0]//2, self.shape[1]//2)
+            half_length = min(center)
+            left = center[1]-half_length
+            right = center[1]+half_length
+            bot = center[0]-half_length
+            top  = center[0]+half_length
+            image = image[left:right, bot:top]
+        
         return image
