5c07f0b08b1a38b2106a95d27192efdeec70470b
svd2802 <narutoche@bk.ru>
2019-11-21 20:18:01 +0600

    Practice 3. Not work with video

--- samples/practice2_sumin_viktor.py
+++ samples/practice2_sumin_viktor.py
@@ -1,42 +1,56 @@
 ﻿"""
-
 Inference engine detector sample
+python practive2_template.py -c ../models/mobilenet-ssd.xml -w ../models/mobilenet-ssd.bin -i ../images/dog.jpg -d CPU -l "C:\Program Files (x86)\IntelSWTools\openvino\inference_engine\bin\intel64\Release\cpu_extension_avx2.dll"
  
 """
 import sys
 import cv2
 import argparse
+import time
 import logging as log
 sys.path.append('../src')
 from ie_detector import InferenceEngineDetector
 
 def build_argparse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', help = 'your cmd \ input ', type = str)
-    parser.add_argument('-w', '--weights', help = 'your cmd \ input ', type = str)
-    parser.add_argument('-i', '--input', help = 'your cmd \ input ', type = str)
-
     
+    parser.add_argument('-c', '--config', help = 'Path to .xml model file', 
+                        type = str)
+    parser.add_argument('-w', '--weights', help = 'Path to .bin model file', 
+                        type = str)
+    parser.add_argument('-l', '--cpu_extension', help = 'Optional. Required for CPU custom layers. Absolute path to a shared library with the kernels implementations.',    
+                        default = '', type = str)
+    parser.add_argument('-d', '--device', help = 'Execution device', 
+                        type=str, default = 'CPU')
+    parser.add_argument('-i', '--input', help = 'Input argument', 
+                        type = str)
     return parser
 
 def main():
-    log.basicConfig(format = "[%(levelname)s] %(message)s", level = log.INFO, 
+    log.basicConfig(format = '[%(levelname)s] %(message)s', level = log.INFO, 
                                stream = sys.stdout)
-    log.info("Hello object detection!")
+    log.info('Hello object detection!')
     args = build_argparse().parse_args()
-    weights = args.weights
-    config = args.config
-    IEDetector = InferenceEngineDetector(weights, config, 'CPU')
+    if args.input:
+        image = cv2.VideoCapture(args.input)
+    else:
+        image = cv2.VideoCapture(0)
+        image = cv2.imread(args.input)
+    log.info('Loaded image with size {}'.format(image.shape))
     
-    image_path = args.input
-    image = cv2.imread(image_path)
-    log.info(image.shape)
-    
-    #IEDetector.detect(image)
+    detector = InferenceEngineDetector(args.weights, args.config, args.device, args.cpu_extension)
+    log.info('Loaded dl net')
+    while True:
+    # Capture frame-by-frame
+        ret, frame = image.read()
+        detect_result = detector.detect(image)
+        detect_image = detector.draw_detection(detect_result, image)
+    log.info('Objects are drown')
 
-    cv2.imshow("Image", IEDetector.detect(image))
+    cv2.imshow('Detections', detect_image)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()    
+    cv2.destroyAllWindows() 
+    
     return 
 
 if __name__ == '__main__':
--- src/ie_detector.py
+++ src/ie_detector.py
@@ -1,64 +1,58 @@
 """
-
 Inference engine detector
  
 """
 import cv2
 import numpy as np
 from openvino.inference_engine import IENetwork, IECore
+import time
 
 class InferenceEngineDetector:
-    def __init__(self, weightsPath = None, configPath = None, device = 'CPU', extension = 
-                 'C:\Program Files (x86)\IntelSWTools\openvino_2019.3.379\inference_engine\bin\intel64\Release\cpu_extension_avx2.dll'):
-        ie = IECore()
+    
+    
+    def __init__(self, weightsPath = None, configPath = None,
+                 device = 'CPU', extension = None):
+        self.weights = weightsPath
+        self.config = configPath
+        self.ie = IECore()
+        self.net = IENetwork(model = configPath, weights = weightsPath)
         if extension:
-            ie.add_extension(extension)
-        net = IENetwork(model = configPath, weights = weightsPath)
-        exec_net = self.ie.load_network(network = self.net, device_name = device)
+            self.ie.add_extension(extension, 'CPU')
+        self.exec_net = self.ie.load_network(network = self.net, device_name = device)
+        
+        return
+
+    def draw_detection(self, detections, img):
         classNames = { 0: 'background',
                       1: 'aeroplane', 2: 'bicycle', 3: 'bird', 4: 'boat',
                       5: 'bottle', 6: 'bus', 7: 'car', 8: 'cat', 9: 'chair',
                       10: 'cow', 11: 'diningtable', 12: 'dog', 13: 'horse',
                       14: 'motorbike', 15: 'person', 16: 'pottedplant',
                       17: 'sheep', 18: 'sofa', 19: 'train', 20: 'tvmonitor' }
-        return
-
-    def draw_detection(self, detections, img):
-        for i in range(detections.shape[2]):
-            confidence = detections[0, 0, i, 2]   
-            if confidence > 0.2: 
-                class_id = int(detections[0, 0, i, 1]) 
-                xLeftBottom = int(detections[0, 0, i, 3] * cols) 
-                yLeftBottom = int(detections[0, 0, i, 4] * rows)
-                xRightTop   = int(detections[0, 0, i, 5] * cols)
-                yRightTop   = int(detections[0, 0, i, 6] * rows)
-                
-                
-                heightFactor = img.shape[0]/300.0  
-                widthFactor = img.shape[1]/300.0 
-                xLeftBottom = int(widthFactor * xLeftBottom) 
-                yLeftBottom = int(heightFactor * yLeftBottom)
-                xRightTop   = int(widthFactor * xRightTop)
-                yRightTop   = int(heightFactor * yRightTop)
-  
-                cv2.rectangle(img, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop), (0, 255, 0))
+        (h, w) = img.shape[:2]
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.5:
+                class_id = detections[0, 0, i, 1]
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                #text = "{}%_{:.2f}%".format(, confidence * 100)
+                #y = startY - 10 if startY - 10 > 10 else startY + 10
+                #cv2.rectangle(img, (startX, startY), (endX, endY), (0, 255, 0), 2)
+                cv2.rectangle(img, (startX, startY), (endX, endY), (0, 255, 0), 2)
                 if class_id in classNames:
-                    label = classNames[class_id] + ": " + str(
-                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-                        yLeftBottom = max(yLeftBottom, labelSize[1])
-                        cv2.rectangle(img, (xLeftBottom, yLeftBottom - labelSize[1]),
-                                     (xLeftBottom + labelSize[0], yLeftBottom + baseLine),
-                                     (255, 255, 255), cv2.FILLED)
-                    cv2.putText(img, label, (xLeftBottom, yLeftBottom), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-
-        cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-        cv2.imshow("frame", img)
+                    label = classNames[class_id]
+                    text = "{:.2f}%".format(confidence * 100)
+                    y = startY - 10 if startY - 10 > 10 else startY + 10
+                    
+                    cv2.putText(img, text + str(label), (startX, y), cv2.FONT_HERSHEY_COMPLEX, 0.45, (0, 0, 255), 1)
+                
         return img
 
     def _prepare_image(self, image, h, w):
+        if image.shape[:-1] != (h, w):
+            image = cv2.resize(image, (w, h))
         image = image.transpose((2, 0, 1))
-        image = cv2.resize(image, dsize = (h, w))
-        
         return image
         
     def detect(self, image):
@@ -66,9 +60,11 @@ class InferenceEngineDetector:
         out_blob = next(iter(self.net.outputs))
         n, c, h, w = self.net.inputs[input_blob].shape
         blob = self._prepare_image(image, h, w)
-        output = self.exec_net.infer(inputs = {input_blob: blob})
-        output = output[out_blob]
-
-        detection = self.draw_detection(output, image)
         
+        
+        
+        start_time = time.time()
+        output = self.exec_net.infer(inputs={input_blob: blob})
+        log.info("FPS: " + str(1/(time.time() - start_time)))
+        detection = output[out_blob]
         return detection
